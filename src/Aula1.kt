c824ef2590df018a3ad5ad56b420c68455f73ab4
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2021-05-05 20:53:09 -0300

    Termino da aula 1

--- src/Aula1.kt
+++ src/Aula1.kt
@@ -54,14 +54,51 @@ fun main() {
 //    petsMutavel.forEach { pet ->
 //        println(pet)
 //    }
-    var x = 0
-    while (x <= 2) {
-        println(x)
-        x++
-    }
+//    var x = 0
+//    while (x <= 2) {
+//        println(x)
+//        x++
+//    }
     /*
      0 x < 2 - 0
      1 x < 2 - 1
      2 x < 2
      */
+
+//    meuNome()
+//    meuNomeCompleto("Cesar", "Rodrigues")
+//    val nome = meuNomeCompletoComRetorno("Eduardo", "Misina")
+//    println(nome)
+    println(questao3(5))
+    questao4()
+}
+
+fun questao3(numero: Int): Boolean {
+    return numero % 2 == 0
+}
+
+fun questao4() {
+    var cont = 0
+    var numero = 1
+    while (cont < 100) {
+        if (numero % 2 != 0) {
+            println(numero)
+            cont++
+        }
+
+        numero++
+    }
+}
+
+fun meuNome() {
+    meuNomeCompleto("Victor", "Motta")
+    println("Cesar Rodrigues")
+}
+
+fun meuNomeCompleto(nome: String, sobrenome: String) {
+    println("$nome $sobrenome")
+}
+
+fun meuNomeCompletoComRetorno(nome: String, sobrenome: String): String {
+    return "$nome $sobrenome"
 }
